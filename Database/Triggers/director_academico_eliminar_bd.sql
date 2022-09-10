c207d8596d0c90908e310e45b63fe4c443d3bc8b
AaronSosaRamos <113047749+AaronSosaRamos@users.noreply.github.com>
2022-09-10 09:52:09 -0500

    Delete Database directory

--- Database/Triggers/director_academico_eliminar_bd.sql
+++
@@ -1,8 +0,0 @@
-DROP TRIGGER IF EXISTS director_academico_eliminar_bd;
-
-DELIMITER //
-CREATE TRIGGER director_academico_eliminar_bd BEFORE DELETE ON infinite_knowledge.director_academico FOR EACH ROW
-BEGIN 
-	DELETE FROM infinite_knowledge.usuario WHERE usuario.dni = OLD.dni;
-END //
-DELIMITER ;
--- Database/Triggers/estudiante_eliminar_bd.sql
+++
@@ -1,8 +0,0 @@
-DROP TRIGGER IF EXISTS estudiante_eliminar_bd;
-
-DELIMITER //
-CREATE TRIGGER estudiante_eliminar_bd BEFORE DELETE ON infinite_knowledge.estudiante FOR EACH ROW
-BEGIN 
-	DELETE FROM infinite_knowledge.usuario WHERE usuario.dni = OLD.dni;
-END //
-DELIMITER ;
